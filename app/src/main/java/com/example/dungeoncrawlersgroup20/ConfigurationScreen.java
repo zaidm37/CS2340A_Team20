a3476faa60bdb8543a0fdd5ca6c293cd2238b1d9
Zaid Mohammed <zam8475@gmail.com>
2023-09-19 11:51:04 -0400

    Create Config Screen

---
+++ app/src/main/java/com/example/dungeoncrawlersgroup20/ConfigurationScreen.java
@@ -0,0 +1,24 @@
+package com.example.dungeoncrawlersgroup20;
+
+import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class ConfigurationScreen extends AppCompatActivity {
+    EditText Name;
+    Button continueButton;
+    TextView textView;
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.configuration_screen);
+        Name = (EditText)findViewById(R.id.Name);
+        continueButton = (Button)findViewById(R.id.continueButton);
+
+    }
+}
