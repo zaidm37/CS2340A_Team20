158694bb4d4007482811bbcccce52647156ddd8b
jjones634 <joshuajones2728@gmail.com>
2023-09-20 16:34:31 -0400

    make sprites clickable

--- app/src/main/java/com/example/dungeoncrawlersgroup20/ConfigScreen.java
+++ app/src/main/java/com/example/dungeoncrawlersgroup20/ConfigScreen.java
@@ -1,9 +1,12 @@
 package com.example.dungeoncrawlersgroup20;
 
 import android.os.Bundle;
+import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.ImageButton;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import androidx.appcompat.app.AppCompatActivity;
 
@@ -11,6 +14,10 @@ public class ConfigScreen extends AppCompatActivity {
     EditText Name;
     Button continueButton;
     TextView textView;
+    ImageButton sprite1;
+    ImageButton sprite2;
+    ImageButton sprite3;
+
 
 
     @Override
@@ -18,6 +25,27 @@ public class ConfigScreen extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.config_screen);
         Name = (EditText)findViewById(R.id.Name);
+        sprite1 = (ImageButton) findViewById(R.id.imageButton);
+        sprite1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Toast.makeText(ConfigScreen.this, "You Selected the First Character", Toast.LENGTH_SHORT).show();
+            }
+        });
+        sprite2 = (ImageButton) findViewById(R.id.imageButton2);
+        sprite2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Toast.makeText(ConfigScreen.this, "You Selected the Second Character", Toast.LENGTH_SHORT).show();
+            }
+        });
+        sprite3 = (ImageButton) findViewById(R.id.imageButton3);
+        sprite3.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Toast.makeText(ConfigScreen.this, "You Selected the Third Character", Toast.LENGTH_SHORT).show();
+            }
+        });
         continueButton = (Button)findViewById(R.id.continueButton);
     }
 }
