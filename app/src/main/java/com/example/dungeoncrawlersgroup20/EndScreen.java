9cbb9c8f1d0ecc14c7948b534d89a2b3ca4952e6
jjones634 <joshuajones2728@gmail.com>
2023-09-20 20:13:40 -0400

    fully finish all screens, all screens should be functioning up to rubric standards

--- app/src/main/java/com/example/dungeoncrawlersgroup20/EndScreen.java
+++ app/src/main/java/com/example/dungeoncrawlersgroup20/EndScreen.java
@@ -10,16 +10,5 @@ public class EndScreen extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.end_screen);
-        Bundle bundle = getIntent().getExtras();
-        String name = bundle.getString("user"); //gets the previous name from config screen
-        int hp = bundle.getInt("diff"); //gets difficulty
-        if (hp == 1) {
-            hp = 300;
-        } else if (hp == 2) {
-            hp = 200;
-        } else if (hp == 3) {
-            hp = 100;
-        } // sets hp depending on difficulty, 1 = easy = more health, etc
-        int character = bundle.getInt("sprite"); //gets which sprite number in drawable was selected
     }
 }
--- app/src/main/java/com/example/dungeoncrawlersgroup20/GameScreen.java
+++ app/src/main/java/com/example/dungeoncrawlersgroup20/GameScreen.java
@@ -2,13 +2,60 @@ package com.example.dungeoncrawlersgroup20;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.TextView;
 
 public class GameScreen extends AppCompatActivity {
+    TextView UserName;
+    TextView Difficulty;
+    ImageView Character;
+    TextView HP;
+    Button Next;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.game_screen);
+        Bundle bundle = getIntent().getExtras();
+        String name = bundle.getString("user"); //gets the previous name from config screen
+        int hp = bundle.getInt("diff"); //gets difficulty
+        String diff = "";
+        if (hp == 1) {
+            hp = 300;
+            diff = "Easy";
+        } else if (hp == 2) {
+            hp = 200;
+            diff = "Medium";
+        } else if (hp == 3) {
+            hp = 100;
+            diff = "Hard";
+        } // sets hp depending on difficulty, 1 = easy = more health, etc
+        int character = bundle.getInt("sprite"); //gets which sprite number in drawable was selected
+        UserName = (TextView) findViewById(R.id.name);
+        UserName.setText(name);
+        Difficulty = (TextView) findViewById(R.id.difficulty);
+        Difficulty.setText(diff);
+        HP = (TextView) findViewById(R.id.health);
+        HP.setText(String.valueOf(hp));
+        Character = (ImageView) findViewById(R.id.character);
+        if (character == 1) {
+            Character.setImageResource(R.drawable.sprite1);
+        } else if (character == 2) {
+            Character.setImageResource(R.drawable.sprite2);
+        } else if (character == 3) {
+            Character.setImageResource(R.drawable.sprite3);
+        }
+        Next = (Button) findViewById(R.id.buttonNext);
+        Next.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent inte = new Intent(GameScreen.this, EndScreen.class);
+                startActivity(inte);
+            }
+        });
     }
 }
