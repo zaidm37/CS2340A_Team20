aec44417077522763a7513688e4e58dc45fdeb85
Joshua Muehring <jmuehring3@gatech.edu>
2023-10-11 13:03:55 -0400

    Added 2 unit tests to test singleton property of Leaderboard

--- app/src/test/java/com/example/dungeoncrawlersgroup20/ExampleUnitTest.java
+++ app/src/test/java/com/example/dungeoncrawlersgroup20/ExampleUnitTest.java
@@ -1,8 +1,12 @@
 package com.example.dungeoncrawlersgroup20;
 
+import org.junit.After;
+import org.junit.Before;
 import org.junit.Test;
 
 import static org.junit.Assert.*;
+
+import java.lang.reflect.Field;
 
 /**
  * Example local unit test, which will execute on the development machine (host).
@@ -14,4 +18,27 @@ public class ExampleUnitTest {
     public void addition_isCorrect() {
         assertEquals(4, 2 + 2);
     }
+
+    @Before
+    @After
+    public void resetSingleton() throws SecurityException, NoSuchFieldException, IllegalArgumentException, IllegalAccessException {
+        Field instance = Leaderboard.class.getDeclaredField("instance");
+        instance.setAccessible(true);
+        instance.set(null, null);
+    }
+    @Test
+    public void testSameSingleton() {
+        Leaderboard leaderboard1 = Leaderboard.getInstance();
+        Leaderboard leaderboard2 = Leaderboard.getInstance();
+        assertSame("not same instance", leaderboard1, leaderboard2);
+    }
+    @Test
+    public void testSameHashCodeSingleton() {
+        Leaderboard leaderboard1 = Leaderboard.getInstance();
+        Leaderboard leaderboard2 = Leaderboard.getInstance();
+        assertEquals(leaderboard1.hashCode(), leaderboard2.hashCode());
+    }
+
+
+
 }
